09c1f36ee01e29c6887fe51103ddfc37c9f58f53
Zia269100 <md.ziakhalid2691@gmail.com>
2025-10-23 16:00:17 +0530

    jgfg

--- app.py
+++ app.py
@@ -138,4 +138,5 @@ def infer():
 # 5️⃣ Run Flask
 # -----------------------------------------------------
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))  # Use Render's assigned port
+    app.run(host="0.0.0.0", port=port)
